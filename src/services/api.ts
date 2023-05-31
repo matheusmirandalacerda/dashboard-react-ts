398d8c4faf2dedf00271c3d19d04de32e6f124aa
Matheus <matheus.miranda@hotmail.com>
2023-05-30 22:16:17 -0300

    Add "api na URL"

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 const api = axios.create({
     //baseURL: 'https://json-server-ex-gq2u.onrender.com',
     //baseURL: 'http://localhost:5000',
-    baseURL: 'https://site-pessoal-api-0f6p.onrender.com'
+    baseURL: 'https://site-pessoal-api-0f6p.onrender.com/api'
 });
 
 export default api;
